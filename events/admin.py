c79b9e7746519b0557efe86a016af36d6044d643
mbeche <mb.johnsonbece@gmail.com>
2019-08-24 18:43:54 -0500

    Commit 3: Create events model. Add events to django admin.

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'presenter', 'time', 'location')
+
+admin.site.register(Event, EventAdmin)
