55c1ebf6521375a19397fe173f80e78adda0f7fd
mbeche <mb.johnsonbece@gmail.com>
2019-08-24 21:48:27 -0500

    Commit 4: Create backend API endpoint for events

---
+++ events/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from .models import Event
+from rest_framework import serializers
+
+
+# serializers to convert from Django model objects to JSON
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('url', 'username', 'email', 'groups')
+
+# or specify all fields
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
--- events/views.py
+++ events/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from .models import Event
+from .serializers import UserSerializer, EventSerializer
+from rest_framework import serializers
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = get_user_model().objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    resource_name = 'events'
+    queryset = Event.objects.all().order_by('-time')
+    serializer_class = EventSerializer
